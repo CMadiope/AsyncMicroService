42c6867b3a489dbc533246871c8b619f265643c3
Falcone <confidencem97@gmail.com>
2024-09-20 14:01:50 +0200

    init

--- ProductAPI/obj/Debug/net7.0/ProductAPI.AssemblyInfo.cs
+++ ProductAPI/obj/Debug/net7.0/ProductAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d9b818cf7625d23a55b8ec62007ae2349be6f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
